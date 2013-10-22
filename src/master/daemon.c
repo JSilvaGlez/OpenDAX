935219af0f7c855852b5f8a72b7a4e7fcabdcab2
Phil Birkelbach <phil@petrasoft.net>
2013-10-22 13:03:42 -0500

    Minor documentation changes.  Fixed lua lib bug in arduino module.

--- src/master/daemon.c
+++ src/master/daemon.c
@@ -15,10 +15,8 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
-
- * This file contains general functions that are used throughout the
- * OpenDAX program for things like logging, error reporting and 
- * memory allocation.
+ *  This file contains the functions used to send the master service
+ *  to the background.
  */
 
 #include <common.h>
